755e13af4928875bee774ad828d3eeb3c640e5ff
vanshverma2006 <vansh.verma18082006@gmail.com>
2025-02-11 20:44:52 +0530

    any redix to binary

--- binaryToDecimal.c
+++ binaryToDecimal.c
@@ -2,6 +2,9 @@
 #include <math.h>
 
 int main() {
+    int type;
+    printf("enter value of redix : ");
+    scanf("%d",&type);
     int n;
     printf("num of digits :: ");
     scanf("%d", &n);
@@ -13,12 +16,35 @@ int main() {
         scanf("%d", &array[i]);
     }
 
-
+if (type==2){
     for (int i = 0; i < n; i++) {
         finalAns += array[n - i - 1] * (int)pow(2, i);   
     }
 
+    printf("binary to Decimal: %d\n", finalAns);
+}
+else if(type==8){
+    for (int i = 0; i < n; i++) {
+        finalAns += array[n - i - 1] * (int)pow(8, i);   
+    }
+
+    printf("octal to Decimal: %d\n", finalAns);
+}
+else if(type==16){
+    for (int i = 0; i < n; i++) {
+        finalAns += array[n - i - 1] * (int)pow(3, i);   
+    }
+
+    printf("hexadecimal to Decimal: %d\n", finalAns);
+}
+else{
+    for (int i = 0; i < n; i++) {
+        finalAns += array[n - i - 1] * (int)pow(type, i);   
+    }
+
     printf("Decimal: %d\n", finalAns);
+}
+
 
     return 0;
 }
